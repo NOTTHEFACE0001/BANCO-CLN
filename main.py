7dcaa674e57e38c7dc5a29c1956b6227acb41c98
NOTTHEFACE0001 <carinjavier19@gmail.com>
2026-05-21 15:38:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -220,7 +220,7 @@ intents.message_content = True
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-guild_obj = discord.Object(id=GUILD_ID)
+guild_obj = discord.Object(id=1486083692089704619)
 
 @bot.event
 async def on_ready():
